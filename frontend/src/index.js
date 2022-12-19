f61c83bdffa8be2e00362ad7482b495dad89a666
Matvey Semenov <matveytri777@gmail.com>
2022-12-19 15:16:20 +0200

    add contract instances & web3 provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,31 @@ import React from "react";
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import Web3 from 'web3';
 import { Provider } from 'react-redux';
 import { Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 
+import config from './config.json';
 import store from './store/store';
 import App from "./App";
+
+
+const NFT_COLLECTIONS = config.NFT_COLLECTIONS;
+
+const _getInstances = () => {
+  let contractInstances = [];
+  for (let name in NFT_COLLECTIONS) {
+    const collection = NFT_COLLECTIONS[name];
+    contractInstances.push(
+      new web3.eth.Contract(collection.ABI, collection.CONTRACT_ADDRESS)
+    )
+  };
+  return contractInstances;
+};
+
+export const web3 = new Web3(Web3.givenProvider);
+export const contractInstances = _getInstances();
 
 
 function getLibrary(provider) {
