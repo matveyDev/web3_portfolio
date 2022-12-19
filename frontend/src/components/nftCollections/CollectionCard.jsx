035e22539fee93b562502bb53747c4e04d39aa89
Matvey Semenov <matveytri777@gmail.com>
2022-12-19 17:56:15 +0200

    simple styled & create card component

--- frontend/src/components/nftCollections/CollectionCard.jsx
+++ frontend/src/components/nftCollections/CollectionCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import './styles/collections.css'
+
+
 
 const CollectionCard = ({data}) => {
+
   return (
     <div className='collection-card'>
-      {data.name}
+      <div className='collection-card__img'>
+        <img src={data.imageURI}/>
+      </div>
+      <div className='collection-card__title' >{data.name}</div>
     </div>
   );
 };
