d6d2406a33f0972554fd05bc1a42bb7c9146e0ec
Matvey Semenov <matveytri777@gmail.com>
2022-12-19 15:11:00 +0200

    add list collection component

--- frontend/src/components/nftCollections/ListCollections.jsx
+++ frontend/src/components/nftCollections/ListCollections.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './styles/collections.css'
+import { contractInstances } from '../../index';
 import CollectionCard from './CollectionCard';
 
 
 const ListCollections = () => {
+  const [nftData, setNftData] = useState([]);
+
+  useEffect(() => {
+    _setNftData();
+  }, []);
+
+  const _setNftData = async () => {
+    contractInstances.map(async (instance) => {
+      const _name = await instance.methods.name().call();
+      const _totalSupply = await instance.methods.totalSupply().call();
+      const _maxSupply = await instance.methods.maxSupply().call();
+
+      setNftData((prevData) => [
+        ...prevData,
+        {
+          name: _name,
+          totalSupply: _totalSupply,
+          maxSupply: _maxSupply
+        }
+      ])
+    });
+  };
+
   return (
     <div className='nft-collections'>
-      <div><CollectionCard name='SADSADSADDAS' symbol='asd' /></div>
-      <div><CollectionCard name='SADSADSADDAS' symbol='asd' /></div>
+      {
+        nftData.map((data, index) => {
+          return (
+            <div key={index}><CollectionCard data={data}/></div>
+          )
+        })
+      }
     </div>
   );
 };
