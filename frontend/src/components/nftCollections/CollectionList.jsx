aadd74d903f08a2db6f620a7a7c313518d3f58e8
Matvey <matveytri777@gmail.com>
2022-12-25 13:57:47 +0200

    changed mint cost logic

---
+++ frontend/src/components/nftCollections/CollectionList.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import { ethers } from 'ethers';
+
+import './styles/collections.css';
+import { contractInstances } from '../../index';
+import CollectionCard from './CollectionCard';
+
+
+const baseURI = 'https://gateway.pinata.cloud/ipfs/';
+
+
+const CollectionList = () => {
+  const [nftData, setNftData] = useState([]);
+
+  useEffect(() => {
+    _setNftData();
+  }, []);
+
+  const _setNftData = async () => {
+    contractInstances.map(async (instance) => {
+      const _name = await instance.methods.name().call();
+      const _totalSupply = await instance.methods.totalSupply().call();
+      const _maxSupply = await instance.methods.maxSupply().call();
+      // First token image
+      const _tokenURI = await instance.methods.tokenURI(1).call();
+      const _imageURI = await getImageUri(_tokenURI);
+
+      let _mintCost = await instance.methods.mintCost().call();
+      _mintCost = ethers.utils.formatEther(_mintCost);
+
+      setNftData((prevData) => [
+        ...prevData,
+        {
+          name: _name,
+          totalSupply: _totalSupply,
+          maxSupply: _maxSupply,
+          imageURI: _imageURI,
+          mintCost: _mintCost,
+          contractInstace: instance,
+        }
+      ])
+    });
+  };
+
+  const getImageUri = async (_tokenURI) => {
+    const url = baseURI + _tokenURI.slice(7);
+    const response = await fetch(url)
+      .then(resp => resp.json())
+      .catch(err => console.log(err))
+
+    const result = baseURI + response.image.slice(7);
+    return result;
+  };
+
+  return (
+    <div className='nft-collections'>
+      {
+        nftData.map((data, index) => {
+          return (
+            <CollectionCard key={index} data={data}/>
+          )
+        })
+      }
+      <div><Outlet props={nftData}/></div>
+    </div>
+  );
+};
+
+export default CollectionList;
